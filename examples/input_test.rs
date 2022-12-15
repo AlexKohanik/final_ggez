e16a84fecd383e258e43c6480107a5b795b13cce
Alexander Kohanik <kohani15@students.rowan.edu>
2022-12-14 19:32:39 -0500

    examples/input_test.rs

--- examples/input_test.rs
+++ examples/input_test.rs
@@ -1,4 +1,6 @@
-//! Example that just prints out all the input events.
+///Documentation updated by: Alexander Kohanik --- December 14th, 2022 --- For further explanation, see youtube video. (Link in github)
+
+// Example that just prints out all the input events.
 
 use ggez::conf;
 use ggez::event::{self, Axis, Button, GamepadId, MouseButton};
@@ -7,6 +9,7 @@ use ggez::graphics::{self, Color, DrawMode};
 use ggez::input::keyboard::{KeyCode, KeyInput};
 use ggez::{Context, GameResult};
 
+///A struct which creates the input test's game state
 struct MainState {
     pos_x: f32,
     pos_y: f32,
@@ -23,6 +26,9 @@ impl MainState {
     }
 }
 
+///Implementing the ggez game state EventHandler trait.
+///The EventHandler trait contains functions pertaining to handling events taking place within our games environment.
+///our update function...Which will define the input taken in and print said key strokes.
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         if ctx.keyboard.is_key_pressed(KeyCode::A) {
@@ -41,6 +47,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///our Draw function will draw on our video game screen, the images you see.
+    ///For this specific example, we focus more on user keystroke/gamepad input testing. Therefore our draw function is very minimal.
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::from([0.1, 0.2, 0.3, 1.0]));
         let rectangle = graphics::Mesh::new_rectangle(
@@ -59,6 +67,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///our Event Handler for when the mouse button down keystroke is input by the user.
+    ///it will record this action and display/print it via terminal window.
     fn mouse_button_down_event(
         &mut self,
         _ctx: &mut Context,
@@ -71,6 +81,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///our mouse button up Event Handler.
+    ///Will record and display/print the release of the mouse button keystroke occuring, within our games environment.
     fn mouse_button_up_event(
         &mut self,
         _ctx: &mut Context,
@@ -83,6 +95,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///Our mouse/cursor movement Event Handler. 
+    ///Records and displays/prints the movement of our cursor amongst our games environment.
     fn mouse_motion_event(
         &mut self,
         _ctx: &mut Context,
@@ -115,11 +129,14 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///Our mouse wheel Event Handler. Displays/prints when the mouse wheel keystroke within our games environment.
     fn mouse_wheel_event(&mut self, _ctx: &mut Context, x: f32, y: f32) -> GameResult {
         println!("Mousewheel event, x: {}, y: {}", x, y);
         Ok(())
     }
 
+    ///Our key down Event Handler. Displays/prints when a key is...
+    ///Pressed and held down, repeated, etc. this function coincides with our next function key up Event Handler.
     fn key_down_event(&mut self, _ctx: &mut Context, input: KeyInput, repeat: bool) -> GameResult {
         println!(
             "Key pressed: scancode {}, keycode {:?}, modifier {:?}, repeat: {}",
@@ -128,6 +145,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///Our KeyUp EventHandler. Displays/prints when a key is...
+    ///Released after being pressed within the games environment.
     fn key_up_event(&mut self, _ctx: &mut Context, input: KeyInput) -> GameResult {
         println!(
             "Key released: scancode {}, keycode {:?}, modifier {:?}",
@@ -136,11 +155,15 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///Text input event handler. Displays/prints text that is input into our games environment. 
     fn text_input_event(&mut self, _ctx: &mut Context, ch: char) -> GameResult {
         println!("Text input: {}", ch);
         Ok(())
     }
 
+    ///If a user is using a gamepad within the game environment, this base needs to be covered also to test input.
+    ///gamepad button down Event Handler records, displays/prints when button/key is pressed, held down, repeated within our games environment.
+    ///This EventHandler function coincides with our next function...gamepad button up event.
     fn gamepad_button_down_event(
         &mut self,
         _ctx: &mut Context,
@@ -151,6 +174,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///Coinciding with our previous function. Records a users input when using a gamepad/controller within our games environment.
+    ///When a button is released, after being held down or pressed. Displays/prints input to terminal window.
     fn gamepad_button_up_event(
         &mut self,
         _ctx: &mut Context,
@@ -161,6 +186,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
         Ok(())
     }
 
+    ///Gamepad axis event Event Handler function, handles joystick movements when a gamepad/controller is used within our games environment.
+    ///Records, Displays/prints the joystick movement within the games environment.
     fn gamepad_axis_event(
         &mut self,
         _ctx: &mut Context,
@@ -185,6 +212,9 @@ impl event::EventHandler<ggez::GameError> for MainState {
     }
 }
 
+///Our main function. 
+///ContextBuilder trait establishes the games title as well as author. 
+///Window Configuration settings declared. Here we have the Windowed mode set, other options include fullscreen and windowed borderless.
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("input_test", "ggez").window_mode(
         conf::WindowMode::default()
